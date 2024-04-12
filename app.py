2f226482ea2d7c27d94dfbb992e2fe65ea8d5a98
Hyunsoo Shin <2091126@hansung.ac.kr>
2024-04-12 18:51:55 +0900

    Bug fix.

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from langchain.chat_models import ChatOpenAI
 from langchain.memory import ConversationBufferMemory
 from langchain.chains import ConversationalRetrievalChain
 from htmlTemplates import css, bot_template, user_template
-from langchain.llms import OllAMA
+
 
 
 def get_pdf_text(pdf_docs):
